1b4797dedf002e7e7a1540cf1222b1339ff71088
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2020-09-26 11:21:43 +0200

    Completed insertion sort

---
+++ insertion.py
@@ -0,0 +1,11 @@
+def insertion_sort(a):
+    n = len(a)
+    for i in range(n):
+        j = i
+        while j > 0 and a[j] < a[j - 1]:
+            swap(a, j, j - 1)
+            j = j - 1
+
+
+def swap(a, i, j):
+    a[i], a[j] = a[j], a[i]
